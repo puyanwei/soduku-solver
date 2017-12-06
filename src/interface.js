e6e4f5e26eb78ed5520e073c4f4aeadb6e1fdfad
Puyan Wei <puyanwei@gmail.com>
2017-12-06 18:01:49 +0000

    submit button console logs hello

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,22 @@
 $(document).ready(() => {
-  $("#box1").on("click", () => {
-    $("#box1").append("<b>hello</b>");
+  $("#input-form").submit(event, () => {
+      event.preventDefault();
+       console.log("hello");
   });
 });
 // $('#box1').val()
+// $("#select-city").submit(event => {
+//   event.preventDefault();
+//   var city = $("#current-city").val();
+//   displayWeather(city);
+// });
+//
+// function displayWeather(city) {
+//   var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city;
+//   var token = "&appid=72a46f472cd78125c67ce1ba75151860";
+//   var units = "&units=metric";
+//   $.get(url + token + units, data => {
+//     $("#city-temperature").text(Math.round(data.main.temp));
+//     $("#city").text(data.name);
+//   });
+// }
