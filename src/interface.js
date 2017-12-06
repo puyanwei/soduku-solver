5c7a3993cf500001dc0f23ad188fd1c7c5e10d05
Puyan Wei <puyanwei@gmail.com>
2017-12-06 22:51:08 +0000

    interface inputting by rows and columns via dropdown menus completed

--- src/interface.js
+++ src/interface.js
@@ -1,8 +1,14 @@
 $(document).ready(() => {
   $("#input-form").submit(event, () => {
     event.preventDefault();
-    var choice = $("#dropdown").val();
-    var location = $("#input-cell").val();
-    document.getElementsByTagName("td")[location - 1].innerText = choice;
+
+    var dropdownChoice = $("#dropdown-choice").val();
+    var dropdownRow = $("#dropdown-row").val();
+    var dropdownCol = $("#dropdown-col").val();
+
+    var rows = document.getElementsByTagName("tr");
+    console.log(rows);
+    console.log(dropdownRow);
+    rows[dropdownRow - 1].children[dropdownCol - 1].innerText = dropdownChoice;
   });
 });
